34207fcdc7b75d2d7fb52af8e86b6a8a69a24c5a
李茂林 <772984968@qq.com>
2017-12-05 17:59:49 +0800

    更改文件配置

--- public/index.php
+++ public/index.php
@@ -12,6 +12,8 @@
 // [ 应用入口文件 ]
 
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
+define('APP_PATH', __DIR__ . '/../app/');
+define('CONF_PATH', __DIR__ . '/../app/conf/');//定义配置文件
+define('EXTEND_PATH',__DIR__.'/../app/extend/');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
